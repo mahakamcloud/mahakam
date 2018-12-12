4fa73083bf3044d8768c74758d7f388de0252dfa
Giri Kuncoro <girikuncoro@gmail.com>
2018-12-12 18:57:01 +0700

    Add options for create cluster

--- cmd/mahakam/create_cluster.go
+++ cmd/mahakam/create_cluster.go
@@ -7,6 +7,7 @@ import (
 )
 
 type CreateClusterOptions struct {
+	Name     string
 	NumNodes int
 }
 
@@ -17,6 +18,10 @@ var createClusterCmd = &cobra.Command{
 	Short: "Create kubernetes cluster",
 	Long:  `Create a kubernetes cluster with one command`,
 	Run: func(cmd *cobra.Command, args []string) {
+		if cco.Name == "" {
+			exitWithHelp(cmd, "Please provide name for your cluster.")
+		}
+
 		if err := RunCreateCluster(cco); err != nil {
 			fmt.Printf("RunCreateCluster error: %s", err.Error())
 		}
@@ -24,10 +29,17 @@ var createClusterCmd = &cobra.Command{
 }
 
 func RunCreateCluster(cco *CreateClusterOptions) error {
-	fmt.Println("RunCreateCluster not yet implemented")
+	fmt.Printf("+%v", cco)
+
 	return nil
 }
 
 func init() {
+	// Required flags
+	createClusterCmd.Flags().StringVarP(&cco.Name, "cluster-name", "c", "", "Name for your kubernetes cluster")
+
+	// Optional flags
+	createClusterCmd.Flags().IntVarP(&cco.NumNodes, "num-nodes", "n", 1, "Number of worker nodes you want kubernetes cluster to run")
+
 	createCmd.AddCommand(createClusterCmd)
 }
