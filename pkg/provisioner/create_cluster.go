06296cd3ca0037ecf16bfcb9b40522b0b9086341
qbl <farabi.iqbal@gmail.com>
2018-12-13 15:16:43 +0700

    Added Terraform writer to create_cluster

--- pkg/provisioner/create_cluster.go
+++ pkg/provisioner/create_cluster.go
@@ -2,12 +2,31 @@ package provisioner
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 
 	"github.com/mahakamcloud/mahakam/pkg/api/v1/models"
+	"github.com/mahakamcloud/mahakam/pkg/terraform/writers"
 )
 
 // CreateCluster creates cluster
 func CreateCluster(cluster *models.Cluster) error {
 	fmt.Println("Creating cluster...")
+
+	backendWriter := &writers.AbstractWriter{&writers.BackendWriter{}}
+	backendWriter.Start()
+
+	return nil
+}
+
+func copyTerraformFiles(src, dst string) error {
+	files, err := ioutil.ReadDir(src)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, f := range files {
+		fmt.Println(f.Name())
+	}
 	return nil
 }
