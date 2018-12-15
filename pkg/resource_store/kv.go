7db6087a9af67bdc5e7b26858cd51e0b500f4107
Giri Kuncoro <girikuncoro@gmail.com>
2018-12-16 00:29:58 +0700

    Rename libkv file and implement Add method for kv store

---
+++ pkg/resource_store/kv.go
@@ -0,0 +1,64 @@
+package resourcestore
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/docker/libkv/store"
+)
+
+type kvResourceStore struct {
+	store store.Store
+}
+
+// NewKVResourceStore creates key value store for resources
+func NewKVResourceStore(s store.Store) ResourceStore {
+	return &kvResourceStore{
+		store: s,
+	}
+}
+
+func (kvr *kvResourceStore) Add(r Resource) (id string, err error) {
+	if err := r.PreCheck(); err != nil {
+		return "", fmt.Errorf("KV resource precheck failed: %s", err)
+	}
+
+	// TODO(giri): check if key exists or duplicated
+	key := r.BuildKey()
+
+	value, err := json.Marshal(r.BuildResource())
+	if err != nil {
+		return "", fmt.Errorf("Add KV resource serialization error: %s", err)
+	}
+
+	opts := &store.WriteOptions{
+		IsDir: false,
+	}
+	_, res, err := kvr.store.AtomicPut(key, value, nil, opts)
+	if err != nil {
+		return "", fmt.Errorf("Add KV resource atomic put error: %s", err)
+	}
+
+	r.GetResource().Revision = res.LastIndex
+	return r.GetResource().ID, nil
+}
+
+func (kvr *kvResourceStore) Get(owner string, key string, resource Resource) error {
+	fmt.Println("libkvResourceStore Get method not implemented")
+	return nil
+}
+
+func (kvr *kvResourceStore) List(owner string, resources interface{}) error {
+	fmt.Println("libkvResourceStore List method not implemented")
+	return nil
+}
+
+func (kvr *kvResourceStore) Update(resource Resource) (revision int64, err error) {
+	fmt.Println("libkvResourceStore Update method not implemented")
+	return 0, nil
+}
+
+func (kvr *kvResourceStore) Delete(owner string, id string, resource Resource) error {
+	fmt.Println("libkvResourceStore Delete method not implemented")
+	return nil
+}
