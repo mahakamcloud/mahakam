01bfb2c0f67a4ab230af08234dcc7b98baf55f3c
Giri Kuncoro <girikuncoro@gmail.com>
2018-12-18 10:59:51 +0700

    Add config module to load mahakam from config yaml file

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,5 +1,12 @@
 package config
 
+import (
+	"fmt"
+
+	"github.com/mahakamcloud/mahakam/pkg/utils"
+	yaml "gopkg.in/yaml.v2"
+)
+
 // Some config constants or environment variables will go away
 // once we populate this through kind of config.yaml
 const (
@@ -10,14 +17,54 @@ const (
 
 // Config represents mahakam configuration
 type Config struct {
-	KVStoreConfig StorageBackendConfig
+	KVStoreConfig StorageBackendConfig `yaml:"storage_backend"`
+}
+
+// LoadConfig loads a configuration file
+func LoadConfig(configFilePath string) (*Config, error) {
+	var config *Config
+	if configFilePath == "" {
+		return config, fmt.Errorf("Must provide non-empty configuration file path")
+	}
+
+	bytes, err := utils.ReadFile(configFilePath)
+	if err != nil {
+		return config, err
+	}
+
+	if err = yaml.Unmarshal(bytes, &config); err != nil {
+		return config, fmt.Errorf("Error unmarshaling configuration file: %s", err)
+	}
+
+	if err = config.Validate(); err != nil {
+		return config, fmt.Errorf("Error validating configuration file: %s", err)
+	}
+
+	return config, nil
+}
+
+// Validate validates mahakam configuration
+func (c *Config) Validate() error {
+	if err := c.KVStoreConfig.Validate(); err != nil {
+		return fmt.Errorf("Error validating KV store configuration: %s", err)
+	}
+	return nil
 }
 
 // StorageBackendConfig stores metadata for storage backend that we use
 type StorageBackendConfig struct {
-	BackendType string
-	Address     string
-	Username    string
-	Password    string
-	Bucket      string
+	BackendType string `yaml:"backend_type"`
+	Address     string `yaml:"address"`
+	Username    string `yaml:"username"`
+	Password    string `yaml:"password"`
+	Bucket      string `yaml:"bucket"`
+}
+
+// Validate validates storage backend configuration
+func (sbc *StorageBackendConfig) Validate() error {
+	if sbc.Address == "" {
+		return fmt.Errorf("Must provide non-empty storage backend address")
+	}
+
+	return nil
 }
---
+++ pkg/config/config_test.go
@@ -0,0 +1,66 @@
+package config_test
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	yaml "gopkg.in/yaml.v2"
+
+	. "github.com/mahakamcloud/mahakam/pkg/config"
+)
+
+var (
+	validConfig = Config{
+		KVStoreConfig: StorageBackendConfig{
+			BackendType: "fake-storage-backend-type",
+			Address:     "fake-storage-backend-address",
+			Username:    "fake-storage-backend-username",
+			Password:    "fake-storage-backend-password",
+			Bucket:      "fake-storage-backend-bucket",
+		},
+	}
+)
+
+var _ = Describe("LoadConfig", func() {
+	var (
+		err    error
+		input  Config
+		output *Config
+
+		dir             string
+		configFilePath  string
+		configFileBytes []byte
+	)
+
+	BeforeEach(func() {
+		input = validConfig
+		dir, err = ioutil.TempDir("", "mahakam-config-")
+		Expect(err).ToNot(HaveOccurred())
+		configFilePath = filepath.Join(dir, "config-file")
+	})
+
+	JustBeforeEach(func() {
+		output, err = LoadConfig(configFilePath)
+	})
+
+	AfterEach(func() {
+		os.RemoveAll(dir)
+	})
+
+	Context("when having valid config file bytes", func() {
+		BeforeEach(func() {
+			configFileBytes, err = yaml.Marshal(input)
+			Expect(err).ToNot(HaveOccurred())
+			err = ioutil.WriteFile(configFilePath, configFileBytes, 0644)
+			Expect(err).ToNot(HaveOccurred())
+		})
+
+		It("returns config file bytes", func() {
+			Expect(err).ToNot(HaveOccurred())
+			Expect(*output).To(Equal(input))
+		})
+	})
+})
