5ebaf8c39454809442b3182b5ebaae49413b5576
Himani Agrawal <himani93@gmail.com>
2018-12-18 18:27:54 +0700

    [Himani|Iqbal] Added resource store for VM

---
+++ pkg/resource_store/resource_vm.go
@@ -0,0 +1,17 @@
+package resourcestore
+
+// ResourceTerraformBackend represents stored resource with terraform backend kind
+type ResourceVM struct {
+	Name                  string
+	IPAddress             string
+	MacAddress            string
+	Host                  string
+	ImageSourcePath       string
+	DNSDhcpServerUsername string
+	UserData              string
+}
+
+// NewResourceTerraformBackend creates new resource cluster
+func NewResourceVM(name string) *ResourceVM {
+	return &ResourceVM{}
+}
