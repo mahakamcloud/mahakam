5ff76d460fc8142850848045587cb555c79792fa
Himani Agrawal <himani93@gmail.com>
2018-12-21 14:18:19 +0700

    [Himani|Iqbal] Added unit test for data.tf parser

--- pkg/terraform/parsers/parser_test.go
+++ pkg/terraform/parsers/parser_test.go
@@ -10,21 +10,35 @@ import (
 
 var _ = Describe("TerraformParser", func() {
 	var (
-		tfParser TerraformParser
+		tfBackendParser TerraformParser
+		tfDataParser    TerraformParser
 	)
 
 	BeforeEach(func() {
-		var backendData = map[string]string{
+		var backendTfData = map[string]string{
 			"Bucket": "tf-mahakam",
 			"Key":    "gofinance-k8s/control-plane/terraform.tfstate",
 			"Region": "ap-southeast-1",
 		}
 
-		tfParser = TerraformParser{
+		var userData = map[string]string{
+			"Bucket": "tf-mahakam",
+			"Key":    "gofinance-k8s/control-plane/terraform.tfstate",
+			"Region": "ap-southeast-1",
+		}
+
+		tfBackendParser = TerraformParser{
 			"backend",
 			templates.Backend,
 			"/tmp/mahakam/terraform/backend.tf",
-			backendData,
+			backendTfData,
+		}
+
+		tfDataParser = TerraformParser{
+			"data",
+			templates.UserData,
+			"/tmp/mahakam/terraform/data.tf",
+			userData,
 		}
 	})
 
@@ -36,11 +50,33 @@ var _ = Describe("TerraformParser", func() {
     }
 }`
 
+	parsedUserData := `data "template_file" "user_data" {
+    template = "${file("${path.module}/templates/user_data.tpl")}"
+
+    vars {
+        hostname                 = "${var.hostname}"
+        dns_dhcp_server_username = "${var.dns_dhcp_server_username}"
+        password                 = "${var.password}"
+        ip_address               = "${var.ip_address}"
+        netmask                  = "${var.netmask}"
+        gate_nss_api_key         = "${var.gate_nss_api_key}"
+        dns_address              = "${var.dns_address}"
+        #CUSTOM_PARAMETERS
+    }
+}`
+
 	Describe("Parsing Templates", func() {
 		Context("With backend.tf data", func() {
-			It("should be able to parse templates.Backend and return string", func() {
-				result := tfParser.ParseTemplate()
+			It("should be able to parse templates.Backend and return the correct string", func() {
+				result := tfBackendParser.ParseTemplate()
 				Expect(result).To(Equal(parsedBackendData))
+			})
+		})
+
+		Context("With data.tf data", func() {
+			It("should be able to parse templates.Data and return the correct string", func() {
+				result := tfDataParser.ParseTemplate()
+				Expect(result).To(Equal(parsedUserData))
 			})
 		})
 	})
