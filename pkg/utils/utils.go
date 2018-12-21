44c9ba76504811c2ef41fb2109e4e2d8f95ea7c6
Giri Kuncoro <girikuncoro@gmail.com>
2018-12-22 01:56:20 +0700

    Add copy file util to copy from source to destination

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net"
 	"os"
@@ -36,4 +37,30 @@ func ReadFile(filePath string) ([]byte, error) {
 // i.e. from 1.2.3.4/16 to 1.2.3.4-16
 func CidrToKeyString(cidr net.IPNet) string {
 	return strings.Replace(cidr.String(), "/", "-", -1)
+}
+
+// CopyFile copies source file to destination
+func CopyFile(src, dst string) (int64, error) {
+	sourceFileStat, err := os.Stat(src)
+	if err != nil {
+		return 0, err
+	}
+
+	if !sourceFileStat.Mode().IsRegular() {
+		return 0, fmt.Errorf("%s is not a regular file", src)
+	}
+
+	source, err := os.Open(src)
+	if err != nil {
+		return 0, err
+	}
+	defer source.Close()
+
+	destination, err := os.Create(dst)
+	if err != nil {
+		return 0, err
+	}
+	defer destination.Close()
+	nBytes, err := io.Copy(destination, source)
+	return nBytes, err
 }
