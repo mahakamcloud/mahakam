27c144f21df0eda86078d5eac07f47e1ec40d2e9
Giri Kuncoro <girikuncoro@gmail.com>
2018-12-25 21:59:12 +0700

    Remove terraform parser from create cluster provisioner

--- pkg/provisioner/create_cluster.go
+++ pkg/provisioner/create_cluster.go
@@ -6,37 +6,11 @@ import (
 	"log"
 
 	"github.com/mahakamcloud/mahakam/pkg/api/v1/models"
-	rs "github.com/mahakamcloud/mahakam/pkg/resource_store/resource"
-	"github.com/mahakamcloud/mahakam/pkg/terraform/parsers"
-	"github.com/mahakamcloud/mahakam/pkg/terraform/templates"
-	tw "github.com/mahakamcloud/mahakam/pkg/terraform/writers"
 )
 
 // CreateCluster creates cluster
 func CreateCluster(cluster *models.Cluster) error {
 	fmt.Println("Creating cluster...")
-
-	var data = map[string]string{
-		"Bucket": "tf-mahakam",
-		"Key":    "gofinance-k8s/control-plane/terraform.tfstate",
-		"Region": "ap-southeast-1",
-	}
-	terraformResource := rs.NewResourceTerraform("backend.tf", data)
-
-	backendParser := parsers.TerraformParser{
-		Name:        "backend",
-		Source:      templates.Backend,
-		Destination: terraformResource.GetName(),
-		Data:        terraformResource.GetAttributes(),
-	}
-	backendTf := backendParser.ParseTemplate()
-
-	tfWriter := tw.TerraformWriter{
-		Data:          backendTf,
-		DestDirectory: "/tmp/mahakam/terraform/",
-		DestFile:      "backend.tf",
-	}
-	tfWriter.WriteFile()
 
 	return nil
 }
