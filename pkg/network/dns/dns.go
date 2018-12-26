1a00766b449b2994a5f96206868630fa634cb93b
vjdhama <vjdhama26@gmail.com>
2018-12-26 16:46:02 +0700

    [Vijay|Iqbal] Added reverse zone file entry generator

--- pkg/network/dns/dns.go
+++ pkg/network/dns/dns.go
@@ -16,13 +16,35 @@ type DNSConfig struct {
 }
 
 const (
-	zoneFileRecordTemplate = `{{.Hostname}} A {{.PrivateIP}}`
+	zoneFileRecordTemplate        = `{{.Hostname}} A {{.PrivateIP}}`
+	reverseZoneFileRecordTemplate = `{{$privateIP := .PrivateIP}}{{$arrPrivateIP := split $privateIP.String "."}}{{index $arrPrivateIP 3}} PTR {{.Hostname}}.{{.DNSZoneName}}.`
+)
+
+var (
+	funcs = template.FuncMap{"split": strings.Split}
 )
 
 // GenerateZoneFileHostRecord returns a record to be appended in zone file
 func (d DNSConfig) GenerateZoneFileHostRecord() string {
 	dnsTemplVal := template.New("dns")
 	tmpl, err := dnsTemplVal.Parse(zoneFileRecordTemplate)
+	if err != nil {
+		log.Fatal("Parse: ", err)
+	}
+
+	var data strings.Builder
+	err = tmpl.Execute(&data, d)
+	if err != nil {
+		log.Fatal("Execute: ", err)
+	}
+
+	return data.String()
+}
+
+// GenerateReverseZoneFileHostRecord returns a record to be appended in zone file
+func (d DNSConfig) GenerateReverseZoneFileHostRecord() string {
+	dnsTemplVal := template.New("dns").Funcs(funcs)
+	tmpl, err := dnsTemplVal.Parse(reverseZoneFileRecordTemplate)
 	if err != nil {
 		log.Fatal("Parse: ", err)
 	}
--- pkg/network/dns/dns_test.go
+++ pkg/network/dns/dns_test.go
@@ -15,15 +15,23 @@ var _ = Describe("DNS", func() {
 
 	BeforeEach(func() {
 		dnsConfig = DNSConfig{
-			PrivateIP: net.ParseIP("10.30.11.254"),
-			Hostname:  "mahakam-dns-01",
+			PrivateIP:   net.ParseIP("10.10.10.1"),
+			DNSZoneName: "sample.domain.io",
+			Hostname:    "mahakam-dns-01",
 		}
 	})
 
 	Describe("Generate zone file host record", func() {
 		It("should return the correct zone file host record", func() {
 			result := dnsConfig.GenerateZoneFileHostRecord()
-			Expect(result).To(Equal("mahakam-dns-01 A 10.30.11.254"))
+			Expect(result).To(Equal("mahakam-dns-01 A 10.10.10.1"))
+		})
+	})
+
+	Describe("Generate reverse zone file host record", func() {
+		It("should return the correct reverse zone file host record", func() {
+			result := dnsConfig.GenerateReverseZoneFileHostRecord()
+			Expect(result).To(Equal("1 PTR mahakam-dns-01.sample.domain.io."))
 		})
 	})
 })
