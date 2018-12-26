6309056b9115cb075250d9aead4ee58d5818b66e
vjdhama <vjdhama26@gmail.com>
2018-12-26 15:27:24 +0700

    [Vijay|Iqbal] Added DNS zone file record generator

---
+++ pkg/network/dns/dns.go
@@ -0,0 +1,37 @@
+package dns
+
+import (
+	"log"
+	"net"
+	"strings"
+
+	"github.com/alecthomas/template"
+)
+
+type DNSConfig struct {
+	PrivateIP          net.IP
+	DNSZoneName        string
+	DNSReverseZonename string
+	Hostname           string
+}
+
+const (
+	zoneFileRecordTemplate = `{{.Hostname}} A {{.PrivateIP}}`
+)
+
+// GenerateZoneFileHostRecord returns a record to be appended in zone file
+func (d DNSConfig) GenerateZoneFileHostRecord() string {
+	dnsTemplVal := template.New("dns")
+	tmpl, err := dnsTemplVal.Parse(zoneFileRecordTemplate)
+	if err != nil {
+		log.Fatal("Parse: ", err)
+	}
+
+	var data strings.Builder
+	err = tmpl.Execute(&data, d)
+	if err != nil {
+		log.Fatal("Execute: ", err)
+	}
+
+	return data.String()
+}
---
+++ pkg/network/dns/dns_test.go
@@ -0,0 +1,29 @@
+package dns_test
+
+import (
+	"net"
+
+	. "github.com/mahakamcloud/mahakam/pkg/network/dns"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("DNS", func() {
+	var (
+		dnsConfig DNSConfig
+	)
+
+	BeforeEach(func() {
+		dnsConfig = DNSConfig{
+			PrivateIP: net.ParseIP("10.30.11.254"),
+			Hostname:  "mahakam-dns-01",
+		}
+	})
+
+	Describe("Generate zone file host record", func() {
+		It("should return the correct zone file host record", func() {
+			result := dnsConfig.GenerateZoneFileHostRecord()
+			Expect(result).To(Equal("mahakam-dns-01 A 10.30.11.254"))
+		})
+	})
+})
