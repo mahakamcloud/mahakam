acd7a9302a52a8ea42de65cbc3302de009527c38
qbl <farabi.iqbal@gmail.com>
2019-01-14 16:48:19 +0700

    Added unit test for NewCheckNode
    Co-authored-by: Giri Kuncoro <giri.kuncoro@go-jek.com>

--- pkg/provisioner/create_cluster_test.go
+++ pkg/provisioner/create_cluster_test.go
@@ -112,3 +112,49 @@ func TestCheckClusterNetworkNodes(t *testing.T) {
 		})
 	}
 }
+
+func TestCheckNode(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	tests := []struct {
+		name        string
+		nodeIP      net.IP
+		expectError error
+		expectReady bool
+	}{
+		{
+			name:        "test check node where node is ready",
+			nodeIP:      net.ParseIP("1.2.3.4"),
+			expectError: nil,
+			expectReady: true,
+		},
+		{
+			name:        "test check node where node is not ready",
+			nodeIP:      net.ParseIP("1.2.3.4"),
+			expectError: fmt.Errorf("timeout waiting for node to be up"),
+			expectReady: false,
+		},
+	}
+
+	l := nilLogger()
+	pc := utils.NewMockPingChecker(ctrl)
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			pc.EXPECT().ICMPPingNWithDelay(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).
+				Return(test.expectReady)
+
+			c := NewCheckNode(test.nodeIP, l, pc)
+			err := c.Run()
+
+			if err != nil {
+				assert.Error(t, err, test.expectError)
+			}
+
+			if err == nil {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
