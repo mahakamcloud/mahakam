f2ca668c212cda1188dbaea042e5521a8ecf31e3
vjdhama <vjdhama26@gmail.com>
2019-01-14 15:26:37 +0530

    Removed irrelevant test

--- pkg/scheduler/scheduler_test.go
+++ pkg/scheduler/scheduler_test.go
@@ -1,30 +1,12 @@
 package scheduler_test
 
 import (
-	"net"
 	"testing"
 
 	"github.com/mahakamcloud/mahakam/pkg/config"
 	"github.com/mahakamcloud/mahakam/pkg/scheduler"
 	"github.com/stretchr/testify/assert"
 )
-
-func TestGetHostReturnsFirstHost(t *testing.T) {
-	host1 := config.Host{Name: "i-test-01", IPAddress: "127.0.0.1"}
-	host2 := config.Host{Name: "i-test-02", IPAddress: "127.0.1.1"}
-
-	hostConfig := []config.Host{
-		host1,
-		host2,
-	}
-
-	scheduledHost, err := scheduler.GetHost(hostConfig)
-
-	expectedHost := net.ParseIP("127.0.0.1")
-
-	assert.Nil(t, err)
-	assert.Equal(t, scheduledHost, expectedHost, "they should be equal")
-}
 
 func TestGetHostReturnsErrorForEmptyHostList(t *testing.T) {
 
