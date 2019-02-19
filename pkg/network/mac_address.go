5e7d1209bcb47aebd5dcf09467eac45c2a52da4a
Himani Agrawal <himani93@gmail.com>
2019-02-19 20:03:46 +0530

    Fix mac address generation to follow libvirt convention

--- pkg/network/mac_address.go
+++ pkg/network/mac_address.go
@@ -9,5 +9,8 @@ func GenerateMacAddress() string {
 	buf := make([]byte, 6)
 	rand.Read(buf)
 	buf[0] = (buf[0] | 2) & 0xfe // Set local bit, ensure unicast address
+	if buf[0] == 0xfe {
+		buf[0] = 0xfa // 0xFE is a reserved first octet in Libvirt, therefore we set the first octet to 0xFA which follows local and unicast address convention
+	}
 	return fmt.Sprintf("%02x:%02x:%02x:%02x:%02x:%02x", buf[0], buf[1], buf[2], buf[3], buf[4], buf[5])
 }
