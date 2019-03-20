66467165d2e780021b3c8abf791f537d06443565
vjdhama <vjdhama26@gmail.com>
2019-03-21 00:41:54 +0700

    updated provisioner to use resource Node

--- pkg/provisioner/types.go
+++ pkg/provisioner/types.go
@@ -1,10 +1,8 @@
 package provisioner
 
-import (
-	"github.com/mahakamcloud/mahakam/pkg/node"
-)
+import "github.com/mahakamcloud/mahakam/pkg/resource_store/resource"
 
 // Provisioner defines sets of methods to be called to cloud provider
 type Provisioner interface {
-	CreateNode(config node.NodeCreateConfig) error
+	CreateNode(node resource.Node) error
 }
